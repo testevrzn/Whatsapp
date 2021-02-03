4854d0419fb00db6cb893e83f510d727093bf982
testevrzn <78117358+testevrzn@users.noreply.github.com>
2021-02-03 09:23:38 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,60 @@
+const help = (prefix) => {
+	return `
+	   🐼 *BOT VRAUZIN* 🐼
+		  
+	  〘 *INFORMAÇÕES* 〙
+➤ Prefix: 「  ${prefix}  」
+➤ Meu Dono: *Vrauzin*
+➤ Contato : +55 (98) 8501-8147
+-------------------------------------------------------
+	    〘 *MENU SOBRE* 〙
+➤ Comando: ${prefix}blocklist
+➤ Descrição: Bot Mostrar a Lista De Bloqueados
+
+➤ Comando: ${prefix}ping
+➤ Descrição: Bot Mostrar Seu Ping
+-------------------------------------------------------
+	    〘 *MENU CRIAR* 〙
+➤ Comando: ${prefix}sticker
+➤ Descrição: imagem / gif / Vídeo Em Adesivo
+ 
+➤ Comando: ${prefix}toimg
+➤ Descrição: Converter Adesivos Em imagem
+-------------------------------------------------------
+	   〘 *MENU DOWNLOADER* 〙
+➤ Comando: ${prefix}images
+➤ Descrição: imagem Aleatória
+
+➤ Comando: ${prefix}ytmp3
+➤ Descrição: Baixar Musica Em Mp3
+
+➤ Comando: ${prefix}ytmp4
+➤ Descrição: Baixar Video Em Mp4
+-------------------------------------------------------
+		〘 *MENU MEME* 〙
+➤ Comando: ${prefix}meme
+➤ Descrição: Imagens aleatórias de meme
+
+➤ Comando: ${prefix}memeindo
+➤ Descrição: Imagens aleatórias de meme
+-------------------------------------------------------
+		〘 *MENU SOM* 〙
+➤ Comando: ${prefix}play
+➤ Descrição: Baixar Musicas Do Youtube
+➤ Exemplo: play Nome Da Musica
+
+➤ Comando: ${prefix}tts
+➤ Descrição: o bot Gravar áudio
+➤ Exemplo: tts pt Bot Panda
+-------------------------------------------------------
+		〘 *MENU WIBU* 〙
+➤ Comando: ${prefix}pokemon
+➤ Descrição: Fotos Pokemon
+
+➤ Comando: ${prefix}loli
+➤ Descrição: Fotos Loli
+-------------------------------------------------------
+`
+}
+
+exports.help = help
