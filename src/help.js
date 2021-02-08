8fd127ce503682bc2bb7389b2621376f8db99b32
testevrzn <78117358+testevrzn@users.noreply.github.com>
2021-02-08 01:35:10 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,152 +1,82 @@
 const help = (prefix) => {
 	return `
-「 *BOT DO CAIO* 」
+	        ☁ *BOT CAIO* ☁
+-------------------------------------------------------	  
+➜ Prefix: 「  ${prefix}  」
+➜ Meu Dono: *CAIO*
+➜ Contato : [OFF]
+-------------------------------------------------------
+	     （ *MENU SOBRE* ）
+➜ Comando: ${prefix}blocklist
+➜ Descrição: Bot Mostrar a Lista De Bloqueados
 
-◪ *informações*
-  ❏ Prefix: 「  ${prefix}  」
-  ❏ Criador : VRAU
-  ❏ Contato : Wa.me/5519999124606
+➜ Comando: ${prefix}ping
+➜ Descrição: Bot Mostrar Seu Ping
+-------------------------------------------------------
+	    （ *MENU CRIAR* ）
+➜ Comando: ${prefix}sticker
+➜ Descrição: imagem / gif / Vídeo Em Adesivo
+ 
+➜ Comando: ${prefix}toimg
+➜ Descrição: Converter Adesivos Em imagem
+-------------------------------------------------------
+	   （ *MENU DOWNLOADER* ）
+➜ Comando: ${prefix}images
+➜ Descrição: imagem Aleatória
 
-◪ *SOBRE*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
-◪ *FAZER*
-  │
-  ├─ ❏ ${prefix}sticker
-  ├─ ❏ ${prefix}stickergif
-  ├─ ❏ ${prefix}toimg
-  ├─ ❏ ${prefix}tomp3
-  ├─ ❏ ${prefix}bpink
-  ├─ ❏ ${prefix}marvellogo
-  ├─ ❏ ${prefix}snowwrite
-  ├─ ❏ ${prefix}3dtext
-  ├─ ❏ ${prefix}ninjalogo
-  ├─ ❏ ${prefix}water
-  ├─ ❏ ${prefix}firetext
-  ├─ ❏ ${prefix}logowolf
-  ├─ ❏ ${prefix}logowolf2
-  ├─ ❏ ${prefix}phlogo
-  ├─ ❏ ${prefix}glitch
-  ├─ ❏ ${prefix}neonlogo
-  ├─ ❏ ${prefix}neonlogo2
-  ├─ ❏ ${prefix}lionlogo
-  ├─ ❏ ${prefix}jokerlogo
-  ├─ ❏ ${prefix}shadow
-  ├─ ❏ ${prefix}burnpaper
-  ├─ ❏ ${prefix}coffee
-  ├─ ❏ ${prefix}lovepaper
-  ├─ ❏ ${prefix}woodblock
-  ├─ ❏ ${prefix}qowheart
-  ├─ ❏ ${prefix}mutgrass
-  ├─ ❏ ${prefix}undergocean
-  ├─ ❏ ${prefix}woodenboards
-  ├─ ❏ ${prefix}wolfmetal
-  ├─ ❏ ${prefix}metalictglow
-  ├─ ❏ ${prefix}8bit
-  ├─ ❏ ${prefix}ttp
-  ├─ ❏ ${prefix}herrypotter
-  ├─ ❏ ${prefix}pubglogo
-  └─ ❏ ${prefix}quotemaker
-◪ *MEDIA*
-  │
-  ├─ ❏ ${prefix}trendtwit
-  ├─ ❏ ${prefix}randomkpop
-  └─ ❏ ${prefix}ytsearch
-◪ *EDUCATION*
-  │
-  ├─ ❏ ${prefix}wiki
-  ├─ ❏ ${prefix}wikien
-  ├─ ❏ ${prefix}nulis
-  ├─ ❏ ${prefix}quotes
-  ├─ ❏ ${prefix}quotes2
-  └─ ❏ ${prefix}artinama
-◪ *KERANG AJAIB*
-  │
-  ├─ ❏ ${prefix}apakah
-  ├─ ❏ ${prefix}kapankah
-  ├─ ❏ ${prefix}rate
-  └─ ❏ ${prefix}bisakah
-◪ *DOWNLOADER*
-  │
-  ├─ ❏ ${prefix}images
-  ├─ ❏ ${prefix}ytmp3
-  ├─ ❏ ${prefix}ytmp4
-  ├─ ❏ ${prefix}tiktok
-  └─ ❏ ${prefix}joox
-◪ *MEME*
-  │
-  ├─ ❏ ${prefix}meme
-  └─ ❏ ${prefix}memeindo
-◪ *SOM*
-  │
-  ├─ ❏ ${prefix}play
-  └─ ❏ ${prefix}tts
-◪ *MÚSICA*
-  │
-  ├─ ❏ ${prefix}lirik
-  └─ ❏ ${prefix}chord
-◪ *ISLAM*
-  │
-  └─ ❏ ${prefix}quran
-◪ *STALK*
-  │
-  ├─ ❏ ${prefix}tiktokstalk
-  └─ ❏ ${prefix}igstalk
-◪ *WIBU*
-  │
-  ├─ ❏ ${prefix}neonime
-  ├─ ❏ ${prefix}pokemon
-  ├─ ❏ ${prefix}loli
-  ├─ ❏ ${prefix}waifu
-  ├─ ❏ ${prefix}randomanime
-  ├─ ❏ ${prefix}husbu
-  ├─ ❏ ${prefix}husbu2
-  ├─ ❏ ${prefix}wait
-  └─ ❏ ${prefix}nekonime
-◪ *DIVERSÃO*
-  │
-  ├─ ❏ ${prefix}alay
-  ├─ ❏ ${prefix}gantengcek
-  ├─ ❏ ${prefix}watak
-  ├─ ❏ ${prefix}hobby
-  ├─ ❏ ${prefix}game
-  ├─ ❏ ${prefix}bucin
-  ├─ ❏ ${prefix}trust
-  ├─ ❏ ${prefix}dare
-  └─ ❏ ${prefix}simi
-◪ *INFORMAÇÃO*
-  │
-  ├─ ❏ ${prefix}bahasa
-  ├─ ❏ ${prefix}kodenegara
-  ├─ ❏ ${prefix}kbbi
-  ├─ ❏ ${prefix}fakta
-  ├─ ❏ ${prefix}infocuaca
-  ├─ ❏ ${prefix}infogempa
-  ├─ ❏ ${prefix}jadwaltvnow
-  └─ ❏ ${prefix}covid
-◪ *DONO*
-  │
-  ├─ ❏ ${prefix}setprefix
-  ├─ ❏ ${prefix}block
-  ├─ ❏ ${prefix}bc
-  ├─ ❏ ${prefix}bcgc
-  ├─ ❏ ${prefix}clone
-  └─ ❏ ${prefix}clearall
-◪ *OUTROS*
-  │
-  ├─ ❏ ${prefix}send
-  ├─ ❏ ${prefix}wame
-  ├─ ❏ ${prefix}virtex
-  ├─ ❏ ${prefix}exe
-  ├─ ❏ ${prefix}qrcode
-  ├─ ❏ ${prefix}afk
-  ├─ ❏ ${prefix}timer
-  ├─ ❏ ${prefix}fml
-  └─ ❏ ${prefix}fml2
+➜ Comando: ${prefix}ytmp3
+➜ Descrição: Baixar Musica Em Mp3
+
+➜ Comando: ${prefix}ytmp4
+➜ Descrição: Baixar Video Em Mp4
+-------------------------------------------------------
+		（ *MENU MEME* ）
+➜ Comando: ${prefix}meme
+➜ Descrição: Imagens aleatórias de meme
+
+➜ Comando: ${prefix}memeindo
+➜ Descrição: Imagens aleatórias de meme
+-------------------------------------------------------
+		（ *MENU SOM* ）
+➜ Comando: ${prefix}play
+➜ Descrição: Baixar Musicas Do Youtube
+➜ Exemplo: play Nome Da Musica
+
+➜ Comando: ${prefix}tts
+➜ Descrição: o bot Gravar áudio
+➜ Exemplo: tts pt Bot Panda
+-------------------------------------------------------
+		（ *MENU WIBU* ）
+➜ Comando: ${prefix}pokemon
+➜ Descrição: Fotos Pokemon
+
+➜ Comando: ${prefix}loli
+➜ Descrição: Fotos Loli
+
+◪ *COMANDO DOS ADMINS*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}kick
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+-------------------------------------------------------
 `
 }
 
